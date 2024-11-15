036090c47bad8efa11766095489079591ce7deb2
karnojitroy <karnojitroy@gmail.com>
2024-11-15 19:36:44 +0600

    Rhombus section finished with js

--- JS/index.js
+++ JS/index.js
@@ -58,3 +58,64 @@ function calculateRectangleArea() {
         displayRectangleWidthLengthArea.setAttribute('hidden', true);
     }
 }
+// calculate Parallelogram Area
+function calculateParallelogramArea() {
+    // input base
+    const parallelogramBaseInput = document.getElementById('parallelogram-base-input');
+    const parallelogramBaseNumber = parallelogramBaseInput.value;
+    const pBase = parseFloat(parallelogramBaseNumber);
+    parallelogramBaseInput.value = '';
+    // input height
+    const parallelogramHeightInput = document.getElementById('parallelogram-height-input');
+    const parallelogramHeightNumber = parallelogramHeightInput.value;
+    pHeight = parseFloat(parallelogramHeightNumber);
+    parallelogramHeightInput.value = '';
+    //Parallelogram area calculation
+    const parallelogram_area = pBase * pHeight;
+    // Display input base, Height and area after clicking on calculate button
+    const parallelogramBase = document.getElementById('parallelogram-base');
+    parallelogramBase.innerText = pBase;
+    const parallelogramHeight = document.getElementById('parallelogram-height');
+    parallelogramHeight.innerText = pHeight;
+    const parallelogramArea = document.getElementById('parallelogram-area');
+    parallelogramArea.innerText = parallelogram_area.toFixed(2);
+
+    const displayParallelogramBaseHeightArea = document.getElementById('parallelogram-b-h-area-display-container');
+    if (parallelogramArea !== null) {
+        displayParallelogramBaseHeightArea.removeAttribute('hidden');
+    }
+    else {
+        displayParallelogramBaseHeightArea.setAttribute('hidden', true);
+    }
+}
+
+// Calculate Rhombus area
+function rhombusArea() {
+    // input d1
+    const rhombusD1Input = document.getElementById('rhombus-d1-input');
+    const rhombusD1Number = rhombusD1Input.value;
+    const d1 = parseFloat(rhombusD1Number);
+    rhombusD1Input.value = '';
+    // input d2
+    const rhombusD2Input = document.getElementById('rhombus-d2-input');
+    const rhombusD2Number = rhombusD2Input.value;
+    const d2 = parseFloat(rhombusD2Number);
+    rhombusD2Input.value = '';
+    // area calculation
+    const rhombus_area = 0.5 * d1 * d2;
+    // Display  d1, 2 and area after clicking on calculate button
+    const rhombusD1 = document.getElementById('rhombus-d1');
+    rhombusD1.innerText = d1;
+    const rhombusD2 = document.getElementById('rhombus-d2');
+    rhombusD2.innerText = d2;
+    const rhombusArea = document.getElementById('rhombus-area');
+    rhombusArea.innerText = rhombus_area.toFixed(2);
+    const rhombusD1D2AreaContainer = document.getElementById('rhombus-d1-d2-area-display-container');
+    if (rhombusArea !== null) {
+        rhombusD1D2AreaContainer.removeAttribute('hidden');
+    }
+    else {
+        rhombusD1D2AreaContainer.setAttribute('hidden', true);
+    }
+
+}
