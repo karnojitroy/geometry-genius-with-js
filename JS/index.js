aac994894fb5e19bdfa1284b097b35cae84c68a3
karnojitroy <karnojitroy@gmail.com>
2024-11-15 13:42:20 +0600

    Triangle function updated and Rectangle Section finished with js

--- JS/index.js
+++ JS/index.js
@@ -3,25 +3,23 @@ function calculateTriangleArea() {
     // input base
     const triangleBaseInput = document.getElementById('triangle-base-input');
     const triangleBaseNumber = triangleBaseInput.value;
-    const base = parseFloat(triangleBaseNumber);
+    const tBase = parseFloat(triangleBaseNumber);
     // assign value 0 after clicking on calculate button
     triangleBaseInput.value = '';
     // input height
     const triangleHeightInput = document.getElementById('triangle-height-input');
     const triangleHeightNumber = triangleHeightInput.value;
-    const height = parseFloat(triangleHeightNumber);
+    const tHeight = parseFloat(triangleHeightNumber);
     triangleHeightInput.value = '';
     // calculate triangle area
-    const triangle_Area = 0.5 * base * height;
+    const triangle_Area = 0.5 * tBase * tHeight;
     // Display input base, height area after clicking on calculate button
     const triangleBase = document.getElementById('triangle-base');
-    triangleBase.innerText = base;
+    triangleBase.innerText = tBase;
     const triangleHeight = document.getElementById('triangle-height');
-    triangleHeight.innerText = height;
+    triangleHeight.innerText = tHeight;
     const triangleArea = document.getElementById('triangle-area');
-    triangleArea.innerText = triangle_Area;
-
-    // input base, height and calculated  triangle area display
+    triangleArea.innerText = triangle_Area.toFixed(2);
     const displayBaseHeightAreaContainer = document.getElementById('triangle-b-h-area-display-container');
     if (triangleArea !== null) {
         displayBaseHeightAreaContainer.removeAttribute('hidden');
@@ -29,5 +27,34 @@ function calculateTriangleArea() {
     else {
         displayBaseHeightAreaContainer.setAttribute('hidden', true);
     }
+}
+// Calculate Rectangle Area
+function calculateRectangleArea() {
+    // input width
+    const rectangleWidthInput = document.getElementById('rectangle-width-input');
+    const rectangleWidthNumber = rectangleWidthInput.value;
+    const rWidth = parseFloat(rectangleWidthNumber);
+    rectangleWidthInput.value = '';
+    // input rectangle length
+    const rectangleLengthInput = document.getElementById('rectangle-length-input');
+    const rectangleLengthNumber = rectangleLengthInput.value;
+    const rLength = parseFloat(rectangleLengthNumber);
+    rectangleLengthInput.value = '';
+    // rectangle area
+    const rectangle_area = rWidth * rLength;
+    // Display input width, Length and area after clicking on calculate button
+    const rectangleWidth = document.getElementById('rectangle-width');
+    rectangleWidth.innerText = rWidth;
+    const rectangleLength = document.getElementById('rectangle-length');
+    rectangleLength.innerText = rLength;
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = rectangle_area.toFixed(2);
 
+    const displayRectangleWidthLengthArea = document.getElementById('rectangle-w-l-area-display-container');
+    if (rectangleArea !== null) {
+        displayRectangleWidthLengthArea.removeAttribute('hidden');
+    }
+    else {
+        displayRectangleWidthLengthArea.setAttribute('hidden', true);
+    }
 }
